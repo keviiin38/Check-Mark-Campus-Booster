3d6d7629ff4bee15ff8a5c1fca17e0b4402bfa53
Kevin <34242560+keviiin38@users.noreply.github.com>
2018-05-13 21:38:35 +0200

    Hide passwords when typing in INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -140,9 +140,11 @@ esac
 echo -e "All requirements installed !\n"
 
 read -p "ID Booster : " IDBOOSTER
-read -p "Campus Booster password : " SUPINFO_PASSWORD
+read -s -p "Campus Booster password : " SUPINFO_PASSWORD
+echo ''
 read -p "GMail address : " GMAIL_ADDRESS
-read -p "GMail password : " GMAIL_PASSWORD
+read -s -p "GMail password : " GMAIL_PASSWORD
+echo ''
 read -p "Receiver address : " RECEIVER_ADDRESS
 
 sed -i "s/IDBOOSTER =.*/IDBOOSTER = \"$IDBOOSTER\"/" check_mark_campus_booster.py
