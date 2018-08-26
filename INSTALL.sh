89232c197940ba493ce82cba7c8e5db853b1540f
Kevin Delbegue <kevin.delbegue@supinfo.com>
2018-08-26 14:09:02 +0200

    [dev-v2] Updated the INSTALL script

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,154 +8,131 @@ case "$BROWSER" in
     *) exit 1;;
 esac
 
-if dpkg -s python-pip &> /dev/null; then
-    PIP_VERSION="pip"
-elif dpkg -s python3-pip &> /dev/null; then
-    PIP_VERSION="pip3"
+# Check if python3 is installed
+echo -en "[    ] python3 install\r"; sleep 0.5
+if command -v python3 &> /dev/null; then
+    echo -en "\e[32m[ OK ]\e[0m python3 install\n"; sleep 0.5
 else
-    echo "Please install python-pip"
-    read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
-    case "$answer" in
-        [yY] | [yY][eE][sS]) su -c "apt-get install python-pip -y && echo -e \"\npython-pip installed !\n\"" root;;
-        [nN] | [nN][oO]) exit 1 ;;
-        *) exit 1;;
-    esac
-    PIP_VERSION="pip"
-    unset answer
+    echo -en "\e[31m[FAIL]\e[0m python3 install\n"; sleep 0.5
+    echo "Please install python3"
+    exit 1
 fi
 
-if ( ! python3 -c "import bs4" &> /dev/null); then
-    echo "Please install Python module beautifulsoup4"
-    read -p "Would you like to run the install now ? (Yes/No) : " answer
-    case "$answer" in
-        [yY] | [yY][eE][sS]) $PIP_VERSION install --user beautifulsoup4 && echo -e "\nbeautifulsoup4 installed !\n" ;;
-        [nN] | [nN][oO]) exit 1 ;;
-        *) exit 1;;
-    esac
-    unset answer
+# Check if python3-pip is installed
+echo -en "[    ] python3-pip install\r"; sleep 0.5
+if dpkg -s python3-pip &> /dev/null; then
+    echo -en "\e[32m[ OK ]\e[0m python3-pip install\n"; sleep 0.5
+else
+    echo -en "\e[31m[FAIL]\e[0m python3-pip install\n"; sleep 0.5
+    echo "Please install python3-pip"
+    exit 1
 fi
 
-if ( ! python3 -c "import selenium" &> /dev/null); then
-    echo "Please install Python module selenium"
-    read -p "Would you like to run the install now ? (Yes/No) : " answer
-    case "$answer" in
-        [yY] | [yY][eE][sS]) $PIP_VERSION install --user selenium && echo -e "\nselenium installed !\n" ;;
-        [nN] | [nN][oO]) exit 1 ;;
-        *) exit 1;;
-    esac
-    unset answer
+# Check if python3-venv is installed
+echo -en "[    ] python3-venv install\r"; sleep 0.5
+if dpkg -s python3-venv &> /dev/null; then
+    echo -en "\e[32m[ OK ]\e[0m python3-venv install\n"; sleep 0.5
+else
+    echo -en "\e[31m[FAIL]\e[0m python3-venv install\n"; sleep 0.5
+    echo "Please install python3-venv"
+    exit 1
 fi
 
+# Create python3 virtual environment
+echo -en "[    ] python3 virtualenv creation\r"; sleep 0.5
+python3 -m venv ./check-mark-campus-booster-venv
+if [ $? -eq 0 ]; then
+    echo -en "\e[32m[ OK ]\e[0m python3 virtualenv creation\n"; sleep 0.5
+else
+    echo -en "\e[31m[FAIL]\e[0m python3 virtualenv creation\n"; sleep 0.5
+    exit 1
+fi
+
+# Install beautifulsoup4 module with pip
+echo -en "[    ] beautifulsoup4 install\r"; sleep 0.5
+./check-mark-campus-booster-venv/bin/pip install beautifulsoup4 --quiet
+if [ $? -eq 0 ]; then
+    echo -en "\e[32m[ OK ]\e[0m beautifulsoup4 install\n"; sleep 0.5
+else
+    echo -en "\e[31m[FAIL]\e[0m beautifulsoup4 install\n"; sleep 0.5
+    exit 1
+fi
+
+# Install selenium module with pip
+echo -en "[    ] selenium install\r"; sleep 0.5
+./check-mark-campus-booster-venv/bin/pip install selenium --quiet
+if [ $? -eq 0 ]; then
+    echo -en "\e[32m[ OK ]\e[0m selenium install\n"; sleep 0.5
+else
+    echo -en "\e[31m[FAIL]\e[0m selenium install\n"; sleep 0.5
+    exit 1
+fi
+
+# Check if the $BROWSER is installed
+echo -en "[    ] $BROWSER install\r"; sleep 0.5
 case "$BROWSER" in
     firefox)
         if ! [ -e /usr/bin/firefox -o -e /usr/local/bin/firefox ]; then
-            echo "Please install firefox"
-            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
-            case "$answer" in
-                [yY] | [yY][eE][sS]) su -c "apt-get install firefox -y && echo -e \"\nfirefox installed !\n\"" root ;;
-                [nN] | [nN][oO]) exit 1 ;;
-                *) exit 1;;
-            esac
-            unset answer
+            echo -en "\e[31m[FAIL]\e[0m $BROWSER install\n"; sleep 0.5
+            echo "Please install firefox to /usr/bin or /usr/local/bin"
+            exit 1
+        else
+            echo -en "\e[32m[ OK ]\e[0m $BROWSER install\n"; sleep 0.5
         fi ;;
     chromium)
         if ! [ -e /usr/bin/chromium-browser -o -e /usr/local/bin/chromium-browser ]; then
-            echo "Please install chromium-browser"
-            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
-                case "$answer" in
-                [yY] | [yY][eE][sS]) su -c "apt-get install chromium-browser -y && echo -e \"\nchromium-browser installed !\n\"" root ;;
-                [nN] | [nN][oO]) exit 1 ;;
-                *) exit 1;;
-            esac
-            unset answer
+            echo -en "\e[31m[FAIL]\e[0m $BROWSER install\n"; sleep 0.5
+            echo "Please install chromium-browser to /usr/bin or /usr/local/bin"
+            exit 1
+        else
+            echo -en "\e[32m[ OK ]\e[0m $BROWSER install\n"; sleep 0.5
         fi ;;
     chrome)
         if ! [ -e /usr/bin/google-chrome-stable -o -e /usr/local/bin/google-chrome-stable ]; then
-            echo "Please install google-chrome-stable"
-            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
-                case "$answer" in
-                [yY] | [yY][eE][sS])
-                    cd /tmp
-                    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-                    echo
-                    su -c "apt-get install /tmp/google-chrome-stable_current_amd64.deb -y &> /dev/null" root
-                    rm google-chrome-stable_current_amd64.deb
-                    echo -e "\ngoogle-chrome-stable installed !\n"
-                    cd - &> /dev/null ;;
-                [nN] | [nN][oO]) exit 1 ;;
-                *) exit 1;;
-            esac
-            unset answer
-    fi ;;
+            echo -en "\e[31m[FAIL]\e[0m $BROWSER install\n"; sleep 0.5
+            echo "Please install google-chrome-stable to /usr/bin or /usr/local/bin"
+            exit 1
+        else
+            echo -en "\e[32m[ OK ]\e[0m $BROWSER install\n"; sleep 0.5
+        fi ;;
     *) exit 1 ;;
 esac
 
 case "$BROWSER" in
     firefox)
+        echo -en "[    ] geckodriver install\r"; sleep 0.5
         if ! [ -e /usr/bin/geckodriver -o -e /usr/local/bin/geckodriver ]; then
-            echo "Please install geckodriver"
-            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
-            case "$answer" in
-                [yY] | [yY][eE][sS])
-                    cd /tmp
-                    wget https://github.com$(curl -sL https://github.com/mozilla/geckodriver/releases/latest | grep 'linux64' | head -1 | cut -d '"' -f 2) -O geckodriver-linux64.tar.gz
-                    tar -zxvf geckodriver-linux64.tar.gz &> /dev/null
-                    echo
-                    su -c "mv geckodriver /usr/bin/geckodriver" root
-                    if [ $? -eq 1 ]; then
-                        rm geckodriver-linux64.tar.gz && rm geckodriver
-                        exit 1
-                    fi
-                    rm geckodriver-linux64.tar.gz
-                    echo -e "\ngeckodriver installed !\n"
-                    cd - &> /dev/null ;;
-                [nN] | [nN][oO]) exit 1 ;;
-                *) exit 1 ;;
-            esac
+            echo -en "\e[31m[FAIL]\e[0m geckodriver install\n"; sleep 0.5
+            echo "Please run ./INSTALL_GECKODRIVER.sh"
+            exit 1
+         else
+            echo -en "\e[32m[ OK ]\e[0m geckodriver install\n"; sleep 0.5
         fi ;;
     chrome | chromium)
+        echo -en "[    ] chromedriver install\r"; sleep 0.5
         if ! [ -e /usr/bin/chromedriver -o -e /usr/local/bin/chromedriver ]; then
-            echo "Please install chromedriver"
-            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
-            case "$answer" in
-                [yY] | [yY][eE][sS])
-                    cd /tmp
-                    if [ -e /tmp/chromedriver ]; then
-                        rm chromedriver
-                    fi
-                    if [ -e /tmp/chromedriver_linux64.zip ]; then
-                        rm chromedriver_linux64.zip
-                    fi
-                    echo
-                    wget "https://chromedriver.storage.googleapis.com/$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE)/chromedriver_linux64.zip"
-                    echo
-                    unzip chromedriver_linux64.zip &> /dev/null
-                    su -c "mv chromedriver /usr/bin/chromedriver" root
-                    if [ $? -eq 1 ]; then
-                        rm chromedriver_linux64.zip && rm chromedriver
-                        exit 1
-                    fi
-                    rm chromedriver_linux64.zip
-                    echo -e "\nchromedriver installed !\n"
-                    cd - &> /dev/null ;;
-                [nN] | [nN][oO]) exit 1 ;;
-                *) exit 1;;
-            esac
+            echo -en "\e[31m[FAIL]\e[0m chromedriver install\n"; sleep 0.5
+            echo "Please run ./INSTALL_CHROMEDRIVER.sh"
+            exit 1
+        else
+            echo -en "\e[32m[ OK ]\e[0m chromedriver install\n"; sleep 0.5
         fi ;;
     *) exit 1 ;;
 esac
 
+# All requirements installed
+echo -en "\n\e[32m[ OK ] All requirements installed !\e[0m\n\n"; sleep 0.5
 
-echo -e "All requirements installed !\n"
-
-read -p "ID Booster : " IDBOOSTER
-read -s -p "Campus Booster password : " SUPINFO_PASSWORD
+# Get all information for the python script
+read -p "ID Booster               : " IDBOOSTER
+read -s -p "Campus Booster password  : " SUPINFO_PASSWORD
 echo ''
-read -p "GMail address : " GMAIL_ADDRESS
-read -s -p "GMail password : " GMAIL_PASSWORD
+read -p "GMail address            : " GMAIL_ADDRESS
+read -s -p "GMail password           : " GMAIL_PASSWORD
 echo ''
-read -p "Receiver address : " RECEIVER_ADDRESS
+read -p "Receiver address         : " RECEIVER_ADDRESS
 
+# Replace all information in the python script
 sed -i "s/IDBOOSTER =.*/IDBOOSTER = \"$IDBOOSTER\"/" check_mark_campus_booster.py
 sed -i "s/SUPINFO_PASSWORD =.*/SUPINFO_PASSWORD = \"$SUPINFO_PASSWORD\"/" check_mark_campus_booster.py
 sed -i "s/GMAIL_ADDRESS =.*/GMAIL_ADDRESS = \"$GMAIL_ADDRESS\"/" check_mark_campus_booster.py
@@ -163,10 +140,19 @@ sed -i "s/GMAIL_PASSWORD =.*/GMAIL_PASSWORD = \"$GMAIL_PASSWORD\"/" check_mark_c
 sed -i "s/RECEIVER_ADDRESS =.*/RECEIVER_ADDRESS = \"$RECEIVER_ADDRESS\"/" check_mark_campus_booster.py
 sed -i "s/BROWSER = \".*/BROWSER = \"$BROWSER\"/" check_mark_campus_booster.py
 
-IDBOOSTER+="_SUPINFO_MARKS"
-
-cp IDBOOSTER_SUPINFO_MARKS $IDBOOSTER
-
+# Unset all used variables
 unset PIP_VERSION IDBOOSTER SUPINFO_PASSWORD GMAIL_ADDRESS GMAIL_PASSWORD RECEIVER_ADDRESS BROWSER
 
-echo -e "\nInstall finished !"
+# First run of the python script
+echo
+echo -en "[    ] check_mark_campus_booster.py first run\r"; sleep 0.5
+./check_mark_campus_booster.py
+if [ $? -eq 0 ]; then
+    echo -en "\e[32m[ OK ]\e[0m check_mark_campus_booster.py first run\n"; sleep 0.5
+else
+    echo -en "\e[31m[FAIL]\e[0m check_mark_campus_booster.py first run\n"; sleep 0.5
+    exit 1
+fi
+
+# Install finished
+echo -en "\n\e[32m[ OK ] Install finished !\n"; sleep 0.5
